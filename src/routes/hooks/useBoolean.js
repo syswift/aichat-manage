3d5f52e64e08c586d0ec39c8fe508f28d9c257f3
syswift <myworlddai97@gmail.com>
2025-03-06 01:52:18 +1300

    1

---
+++ src/routes/hooks/useBoolean.js
@@ -0,0 +1,24 @@
+import { useState, useCallback } from 'react';
+
+export function useBoolean(initialState = false) {
+  const [value, setValue] = useState(initialState);
+
+  const onTrue = useCallback(() => {
+    setValue(true);
+  }, []);
+
+  const onFalse = useCallback(() => {
+    setValue(false);
+  }, []);
+
+  const onToggle = useCallback(() => {
+    setValue((prev) => !prev);
+  }, []);
+
+  return {
+    value,
+    onTrue,
+    onFalse,
+    onToggle,
+  };
+}
