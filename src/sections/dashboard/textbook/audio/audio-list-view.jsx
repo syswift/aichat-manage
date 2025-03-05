497cb15a075618f5f1a77fdf6d677e6cf13b2af4
syswift <myworlddai97@gmail.com>
2025-03-06 06:30:19 +1300

    1

--- src/sections/dashboard/textbook/audio/audio-list-view.jsx
+++ src/sections/dashboard/textbook/audio/audio-list-view.jsx
@@ -1,9 +1,35 @@
 'use client';
 
-import { BlankView } from 'src/sections/blank/view';
+import Button from '@mui/material/Button';
+
+import { paths } from 'src/routes/paths';
+
+import { DashboardContent } from 'src/layouts/dashboard';
+
+import { Iconify } from 'src/components/iconify';
+import { CustomBreadcrumbs } from 'src/components/custom-breadcrumbs';
 
 export function AudioListView() {
     return (
-        <BlankView title="Page audio" />
+        <DashboardContent>
+            <CustomBreadcrumbs
+            heading="音频"
+            links={[
+                { name: '首页', href: paths.dashboard.root },
+                { name: '教材管理', href: paths.dashboard.textbook.audio },
+                { name: '音频' },
+            ]}
+            action={
+                <Button
+                href={paths.dashboard.root}
+                variant="contained"
+                startIcon={<Iconify icon="mingcute:add-line" />}
+                >
+                New user
+                </Button>
+            }
+            sx={{ mb: { xs: 3, md: 5 } }}
+            />
+        </DashboardContent>
     );
 }
